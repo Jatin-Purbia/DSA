5879168a7d26c6532b440dcc805d12a9aa3c67bf
Jatin Purbia <154995240+Jatin-Purbia@users.noreply.github.com>
2025-12-18 19:04:33 +0530

    Time: 0 ms (100.00%) | Memory: 9.7 MB (32.11%) - LeetSync

---
+++ 118-pascals-triangle/pascals-triangle.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        // DONE
+        vector<vector<int>> final;
+        for(int i=1;i<=numRows;i++){
+            vector<int> temp;
+            long long ans = 1;
+            temp.push_back(ans);
+            for(int j=1;j<i;j++){
+                ans = ans * (i-j);
+                ans = ans / (j);
+                temp.push_back(ans);
+            }
+            final.push_back(temp);
+        }
+        return final;
+    }
+};
