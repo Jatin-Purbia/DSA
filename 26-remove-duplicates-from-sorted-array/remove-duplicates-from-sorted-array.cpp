9fc70ee1805be36d4193c9ed1b7b742c6d32f3c8
Jatin Purbia <154995240+Jatin-Purbia@users.noreply.github.com>
2025-12-20 14:34:09 +0530

    Time: 51 ms (5.43%) | Memory: 22.6 MB (77.65%) - LeetSync

---
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        // map <int,int>mpp;
+        int cnt=0;
+        int el=INT_MIN;
+        int n = nums.size();
+        for(int i=0;i<n;i++){
+            if(cnt==0 || el!=nums[i]){
+                cnt=1;
+                el=nums[i];
+            }
+            else if (nums[i]==el){
+                nums.erase(nums.begin()+i);
+                i--;
+                n--;
+            }
+            else cnt=0;
+        }
+        return nums.size();
+       
+        
+    }
+};
