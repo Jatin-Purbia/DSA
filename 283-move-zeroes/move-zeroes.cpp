231434e228ba3d091e624c4a8bf4536a5a009575
Jatin Purbia <154995240+Jatin-Purbia@users.noreply.github.com>
2025-12-22 19:55:56 +0530

    Time: 0 ms (100.00%) | Memory: 23.9 MB (19.81%) - LeetSync

--- 283-move-zeroes/move-zeroes.cpp
+++ 283-move-zeroes/move-zeroes.cpp
@@ -1,13 +1,11 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int n=nums.size();
-        for(int i=0;i<n;i++){
-            if(nums[i]==0){
-                nums.erase(nums.begin()+i);
-                nums.push_back(0);
-                i--;
-                n--;
+        int j=0;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]!=0){
+                swap(nums[i],nums[j]);
+                j++;
             }
         }
     }
